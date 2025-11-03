00026a290ab2540d83cae583faa9dff53cbf96b1
unknown <User@FNR-05Nov21.ad.uky.edu>
2025-11-03 11:50:59 -0500

    working through edits...

--- Notes_for_next_time.R
+++ Notes_for_next_time.R
@@ -19,3 +19,36 @@
 # Avg#SmShrub
 # Avg#MedShrub
 # Avg#LgShrub
+
+# Let's begin with the woody stem data
+woodystems <-read.csv("./woodystems_raw.csv")
+head(woodystems)
+
+# create blank df to hold data
+big_woody_df <- data.frame(point_id = 0, 
+                           AvgPrism = 0,
+                           AvgNoSmSap = 0,
+                           AvgNoMedSap = 0,
+                           AvgNoLgSap = 0,
+                           AvgNoSmShrub = 0,
+                           AvgNoMedShrub = 0,
+                           AvgNoLgShrub = 0)
+
+for(i in 1:length(unique(woodystems$point_id))){
+  # i = 100
+  
+  woodystems_i <- subset(woodystems, point_id == i)
+  
+  point_id_i = woodystems_i$point_id
+  AvgPrism_i = 0
+  AvgNoSmSap_i = 0
+  AvgNoMedSap_i = 0
+  AvgNoLgSap_i = 0
+  AvgNoSmShrub_i = 0
+  AvgNoMedShrub_i = 0
+  AvgNoLgShrub_i = 0
+
+  
+}
+
+
